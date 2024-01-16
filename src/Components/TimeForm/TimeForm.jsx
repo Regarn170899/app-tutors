b8496e281b175925d5092fd47ac34afff766794f
Desclim <declownmail@gmail.com>
2024-01-16 16:13:58 +0300

    Сделал модальное окно для TimeForm.jsx

--- src/Components/TimeForm/TimeForm.jsx
+++ src/Components/TimeForm/TimeForm.jsx
@@ -1,4 +1,5 @@
-import {Button, Form, Input, Select, TimePicker} from 'antd';
+import {Button, Form, Input, Modal, Select, TimePicker} from 'antd';
+import {useState} from "react";
 const { Option } = Select;
 const config = {
     rules: [
@@ -13,6 +14,16 @@ const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
 };
 const TimeForm = (props) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
     const [form] = Form.useForm();
     const onFinish = (values) => {
         const timeFormat = {
@@ -23,75 +34,78 @@ const TimeForm = (props) => {
         form.resetFields();
     };
     return (
-    <Form
-        form={form}
-        name="basic"
-        labelCol={{
-            span: 8,
-        }}
-        wrapperCol={{
-            span: 16,
-        }}
-        style={{
-            maxWidth: 600,
-        }}
-        initialValues={{
-            remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-    >
-        <Form.Item
-            label="Имя"
-            name="name"
-            rules={[
-                {
-                    required: true,
-                    message: 'Введите имя',
-                },
-            ]}
-        >
-            <Input/>
-        </Form.Item>
-        <Form.Item
-
-            name="subject"
-            label="Предмет"
-            rules={[
-                {required: true,
-                message: 'Выберите предмет'
-                },
-            ]}
-        >
-            <Select
-                placeholder="Выберите предмет"
-                allowClear
-            >
-                <Option value="Russian">Русский язык</Option>
-                <Option value="Math">Математика</Option>
-                <Option value="English">Английский язык</Option>
-            </Select>
-        </Form.Item>
-        <Form.Item
-
-            name="time"
-            label="Время" {...config}
-        >
-            <TimePicker
-            />
-        </Form.Item>
-        <Form.Item
-            wrapperCol={{
-                offset: 8,
-                span: 16,
-            }}
-        >
-            <Button type="primary" htmlType="submit">
-                Сохранить
+        <>
+            <Button type="primary" onClick={showModal}>
+                Запись
             </Button>
-        </Form.Item>
-    </Form>
+            <Modal title="Запись" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                <Form
+                    form={form}
+                    name="basic"
+                    labelCol={{
+                        span: 5,
+                    }}
+                    wrapperCol={{
+                        span: 16,
+                    }}
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <p><Form.Item
+                        label="Имя"
+                        name="name"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Введите имя',
+                            },
+                        ]}
+                    >
+                        <Input/>
+                    </Form.Item></p>
+                    <p><Form.Item
+
+                        name="subject"
+                        label="Предмет"
+                        rules={[
+                            {required: true,
+                                message: 'Выберите предмет'
+                            },
+                        ]}
+                    >
+                        <Select
+                            placeholder="Выберите предмет"
+                            allowClear
+                        >
+                            <Option value="Russian">Русский язык</Option>
+                            <Option value="Math">Математика</Option>
+                            <Option value="English">Английский язык</Option>
+                        </Select>
+                    </Form.Item></p>
+                    <p><Form.Item
+
+                        name="time"
+                        label="Время" {...config}
+                    >
+                        <TimePicker/>
+                    </Form.Item></p>
+                    <p><Form.Item
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Сохранить
+                        </Button>
+                    </Form.Item></p>
+                </Form>
+            </Modal>
+        </>
     )
 };
 export default TimeForm;
