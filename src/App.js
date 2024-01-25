595955ea16158c77ddbd9fd401d153a9483581c0
Regarn170899 <1526497@gmail.com>
2024-01-25 16:39:33 +0300

    Добавлена таблица и меню

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Calendar from "./Components/Calendar/Calendar";
 import TimeForm from "./Components/TimeForm/TimeForm";
 import {useState} from "react";
+import {Route, Router, Routes} from "react-router-dom";
+import CustomMenu from "./Components/CustomMenu/CustomMenu";
+import InformationPage from "./Pages/InformationPage/InformationPage";
 
 function App() {
     const [timeFormResult, setTimeFormResult] = useState({})
@@ -11,19 +14,27 @@ function App() {
 
   return (
     <div className="App">
-      <TimeForm isModalOpen={isModalOpen}
-                setIsModalOpen={setIsModalOpen}
-                currentDate={currentDate}
-                timeFormResult={timeFormResult}
-                setTimeFormResult={setTimeFormResult}/>
+        <CustomMenu/>
+        <Routes>
+            <Route path="/" element={
+                <>
+                <TimeForm isModalOpen={isModalOpen}
+                          setIsModalOpen={setIsModalOpen}
+                          currentDate={currentDate}
+                          timeFormResult={timeFormResult}
+                          setTimeFormResult={setTimeFormResult}/>
+                <Calendar setSuccessfulLessons={setSuccessfulLessons}
+                          successfulLessons={successfulLessons}
+                          setTimeFormResult={setTimeFormResult}
+                          timeFormResult={timeFormResult}
+                          isModalOpen={isModalOpen}
+                          setIsModalOpen={setIsModalOpen}
+                          setCurrentDate={setCurrentDate}/>
+            </>
+            }/>
+            <Route path="/about" element={<InformationPage successfulLessons={successfulLessons}/>}/>
+        </Routes>
 
-      <Calendar setSuccessfulLessons={setSuccessfulLessons}
-                successfulLessons={successfulLessons}
-                setTimeFormResult={setTimeFormResult}
-                timeFormResult={timeFormResult}
-                isModalOpen={isModalOpen}
-                setIsModalOpen={setIsModalOpen}
-                setCurrentDate={setCurrentDate}/>
     </div>
   );
 }
